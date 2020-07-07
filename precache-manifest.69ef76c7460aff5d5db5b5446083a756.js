dfae82475dd73e1543f5a613d86996c6c78f6b7e
sititou70 <sititou70@users.noreply.github.com>
2020-07-07 08:07:14 +0000

    deploy: 90980516f2c1fbb6c026072cb5fb84d6471026b3

---
+++ precache-manifest.69ef76c7460aff5d5db5b5446083a756.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "60ad2979159b177fcb6dddb22410b97e",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4aee7e0070417ab4950a",
+    "url": "./static/css/2.61a570a4.chunk.css"
+  },
+  {
+    "revision": "4aee7e0070417ab4950a",
+    "url": "./static/js/2.7c21216c.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "./static/js/2.7c21216c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e96f71743ffe1e5e82f",
+    "url": "./static/js/main.ece9c34c.chunk.js"
+  },
+  {
+    "revision": "ea23f20a92eb48676342",
+    "url": "./static/js/runtime-main.da64628c.js"
+  }
+]);
